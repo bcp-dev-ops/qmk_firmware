9e776d5f0cbace6fb2b39ec8a03d197572939f22
bcp <example@example.com>
2025-12-01 22:21:18 -0500

    added mouse functionality

--- keyboards/yunzii/al68/keymaps/bcp_supreme_68_percent/combos.c
+++ keyboards/yunzii/al68/keymaps/bcp_supreme_68_percent/combos.c
@@ -1,5 +1,6 @@
 enum combo_events {
     COMBO_ALPHA_QWERTY_DRAGSCROLL,
+    COMBO_ALPHA_QWERTY_ESC_1,
 };
 
 // ---- Combo definition ----
@@ -9,8 +10,15 @@ const uint16_t PROGMEM dragscroll_combo[] = {
     COMBO_END
 };
 
+const uint16_t PROGMEM esc_1_combo[] = {
+    MS_BTN1,
+    KC_1,
+    COMBO_END
+};
+
 combo_t key_combos[] = {
     [COMBO_ALPHA_QWERTY_DRAGSCROLL] = COMBO_ACTION(dragscroll_combo),
+    [COMBO_ALPHA_QWERTY_ESC_1] = COMBO_ACTION(esc_1_combo),
 };
 
 uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(key_combos[0]);
@@ -19,6 +27,8 @@ uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(key_combos[0]);
 bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
     switch (combo_index) {
         case COMBO_ALPHA_QWERTY_DRAGSCROLL:
+            return layer_state_is(ALPHA_QWERTY);
+        case COMBO_ALPHA_QWERTY_ESC_1:
             return layer_state_is(ALPHA_QWERTY);
     }
     return true;
@@ -30,6 +40,11 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         case COMBO_ALPHA_QWERTY_DRAGSCROLL:
             if (pressed) {
                 tap_code(MS_BTN3);
+            }
+            break;
+        case COMBO_ALPHA_QWERTY_ESC_1:
+            if (pressed) {
+                tap_code(MS_BTN2);
             }
             break;
     }
